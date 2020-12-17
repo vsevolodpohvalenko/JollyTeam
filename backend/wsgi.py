ad26c7a5742e5403ced65719cd1fbce3838dc764
Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2020-12-17 19:17:47 +0200

    make it better

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -12,8 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 project_folder = os.path.expanduser('~/textilespot')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 application = get_wsgi_application()
+
