d7a8a8db6ad5184f607dac4d9a5fceea6221e22b
Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2020-12-17 18:22:44 +0200

    make it better

--- accounts/models.py
+++ accounts/models.py
@@ -15,13 +15,25 @@ class UserAccountManager(BaseUserManager):
         user.save()
         return  user
 
+    def create_superuser(self, email, first_name,  password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_superuser=True, first_name=first_name,  **extra_fields)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     username = None
     email = models.EmailField(_('email address'),blank=True, unique=True)
     first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, default="Fatherson")
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name']
 
