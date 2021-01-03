2b4ac3ea76a9745017572ba8bbe42f9b229df132
Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2021-01-03 22:18:22 +0200

    Updates

---
+++ precache-manifest.c264f867935223edcc2ecd7197d1d07c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90827f3d884264138a3b380528d101d8",
+    "url": "/JollyTeam/index.html"
+  },
+  {
+    "revision": "7efac7e34d1836b1302e",
+    "url": "/JollyTeam/static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "878d0c29d5edf17e9f53",
+    "url": "/JollyTeam/static/css/main.50a90be2.chunk.css"
+  },
+  {
+    "revision": "7efac7e34d1836b1302e",
+    "url": "/JollyTeam/static/js/2.e8a66f68.chunk.js"
+  },
+  {
+    "revision": "27f1c73f1dc921bc79aa41830d4cd146",
+    "url": "/JollyTeam/static/js/2.e8a66f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878d0c29d5edf17e9f53",
+    "url": "/JollyTeam/static/js/main.ebe9520f.chunk.js"
+  },
+  {
+    "revision": "139f30730175c5629c72",
+    "url": "/JollyTeam/static/js/runtime-main.0d81f496.js"
+  },
+  {
+    "revision": "f9de2f384d88460c6a30433554949c8b",
+    "url": "/JollyTeam/static/media/background.f9de2f38.jpeg"
+  },
+  {
+    "revision": "b9a79b60266cab3d7d933627f8730e54",
+    "url": "/JollyTeam/static/media/default.b9a79b60.jpg"
+  },
+  {
+    "revision": "562b53e7d09e056d02a5a3ed6c3caf39",
+    "url": "/JollyTeam/static/media/dot-com.562b53e7.jpg"
+  },
+  {
+    "revision": "1628b9d9dd6e3bd7aed779ba31cba2f1",
+    "url": "/JollyTeam/static/media/loading_animation.1628b9d9.gif"
+  },
+  {
+    "revision": "d19fdd8a92ca7dc7b519b0bc7397322f",
+    "url": "/JollyTeam/static/media/login.d19fdd8a.jpg"
+  },
+  {
+    "revision": "9db9d7422140d94242fad98a3878b1f6",
+    "url": "/JollyTeam/static/media/p404.9db9d742.png"
+  },
+  {
+    "revision": "72d8a8239b9952cb9ff11ffd2bfa6952",
+    "url": "/JollyTeam/static/media/thumbnail.72d8a823.jpg"
+  }
+]);
